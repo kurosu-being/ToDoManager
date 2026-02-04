912e82c474b9b1940bc3ac696f492e8fd09a97ef
kurosu <kurosu@beingcorp.co.jp>
2026-02-04 16:50:58 +0900

    アプリの修正
    ・不要なusingの削除

--- ToDoManager/TodoLogicTests/TodoServiceTests.cs
+++ ToDoManager/TodoLogicTests/TodoServiceTests.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
 using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoManager.Models;
 using ToDoManager.Services;
 
